7bfc7972f8cd981957f9315e4f5ef615dcd5b698
Faa93 <36281645+Faa93@users.noreply.github.com>
2018-09-03 22:08:55 -0300

    Clase 4

---
+++ C2018-master/GonzalezFacundo/clasesabado/main.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio_ext.h>  //para que funciona el fpurge(stolin); que sirve para limpiar
+#include <string.h> // para que lea string
+
+
+//Trabajo practico
+//TRABAJAR LA CALCULADORA CON FLOTANTES, EL FACTOREAL SE HACE CON NUMEROS ENTEROS, EL RESULTADO DEBEMOS TRABAJARLO COMO ENTENRO.
+//SE PUEDE TRABAJAR CON ENTEROS.
+
+int main()
+{
+int flag, opcion, resultado;
+float num1, num2;
+/*por cada biblioteca que hagamos tenemos que rear dos archivos, uno .C y el otro .H,
+TENEMOS QUE INCLUIR MATH.H (O EL NOMBRE DE LA BIBLIOTECA .H O .C) PARA QUE VAYA A BUSCAR LA INFO EN ESA LIBRERIA
+INGRESAMOS A NEW > FILE > EMPTY FILE, LE AGREGAMOS NOMBRE Y LE DAMOS A SAVE. (MATH.C  = EJ)
+AGREGAMOS OTRO, VAMOS A FILE, EMPTY FILE, Y CREAMOS MATH.H
+EN EL .H van a estar TODAS LAS FIRMAS
+EN EL .C TODO LO QUE ESTA POR DEBAJO DE MAIN, O SEA, LA IMPLEMENTACION
+
+ENTRE COMILLAS "" ES UN ARCHIVO QUE ESTA LOCALMENTE
+ENTRE <> ES UN ARCHIVO QUE ESTA EN C
+
+O SINO
+
+FILE > NEW > FILE > C HEADER y crea dos archivos
+*/
+do
+{
+    printf("1.Ingrese primer operando\n 2.Ingresar 2do operando\n");
+    printf("\nIngrese una opcion del menu:");
+    scanf("%d", &opcion);
+
+    switch(opcion)
+    {
+        case 1:
+        ingresarNumero(num1):
+        break;
+        case 2:
+        ingresarNumero(num2);
+        break;
+        case 3:
+        printf("Calculando...");
+        break;
+        case 4:
+        printf("Resultados");
+        break;
+        case 5:
+        printf("Saliendo...");
+        break;
+        flag = 0;
+
+    default:
+        printf("Ingrese una opcion valida!\n");
+    }
+
+
+}while(flag !=0);
+
+return 0;
+
+
+}
+int ingresarNumero (float* numero);
+//1 2 3 puntos, DO {
+//
+//4. A B C D
+
+
+//lucianavarela@hotmail.es
+//
---
+++ C2018-master/trabajoPractico/main.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*en el archivo .H van a ir los prototipos (declaraciones) suma resta, multiplicacion, division prototipos
+    en el archivo .C van a ir los cuerpos (definiciones) aca van los codigos completos de lo que
+    y eso compondra el main.c
+
+    nosotros ya inlcuimos las bibliotecas #include <stdio.h> y <stddlib.h> y
+
+    En las las switch(opcion)
+    {
+        case 1:
+        ingresarNumero(num1):
+        break;
+        case 2:
+        ingresarNumero(num2);
+        break;
+        case 3:
+        printf("Calculando...");
+        break;
+        case 4:
+        printf("Resultados");
+        break;
+        case 5:
+        printf("Saliendo...");
+        break;
+        flag = 0;
+
+    default:
+        printf("Ingrese una opcion valida!\n");
+    }
+
+
+}while(flag !=0);switch(opcion)
+    {
+        case 1:
+        ingresarNumero(num1):
+        break;
+        case 2:
+        ingresarNumero(num2);
+        break;
+        case 3:
+        printf("Calculando...");
+        break;
+        case 4:
+        printf("Resultados");
+        break;
+        case 5:
+        printf("Saliendo...");
+        break;
+        flag = 0;
+
+    default:
+        printf("Ingrese una opcion valida!\n");
+    }
+
+
+}while(flag !=0); con <> son aquellas que el compilador va a buscar en
+
+
+    #include "c:\\proyectos\\funciones\\calculadora.h"  RUTA ABSOLUTA(va a ir a buscar la biblioteca en esta ubicacion y solo esta ubicacion, si no esta, no muestra nada)
+
+    .\\ -> m dice en que directorio actualmente esta.
+
+    #include ".\\calculadora.h"  (no importa donde sea que este, va a buscar este archivo)  RUTA
+    #include ".\\bibliotecas\\calculadora.h" -> si tenes varias biblitecas podes pedirle buscarla en la biblioteca.
+
+
+
+
+-------------------------------------------
+RESOLVER FACTOREO SIN USAR RECURSIVIDAD
+
+INT VALOR, RESULTADO=1 , I;
+FOR(I =VALOR ; I>0; I--){
+RESULTADO=RESULTADO*I;
+}
+-------------------------------------------
+    */
+
+
+   int  numeroUno, numeroDos, opcion, flag;
+
+
+    do{
+        printf("Ingrese el primer numero:\n Ingrese el segundo numero:\n");
+        printf("\nIngrese una opcion del menu:");
+        scanf("%d", &opcion);switch(opcion)
+    {
+        case 1:
+        numeroUno (numeroUno):
+        break;
+        case 2:
+        numeroDos (numeroDos);
+        break;
+        case 3:
+        printf("Calculando...");
+        break;
+        case 4:
+        printf("Resultados");
+        break;
+        case 5:
+        printf("Saliendo...");
+        break;
+        flag = 0;
+
+    default:
+        printf("Ingrese una opcion valida!\n");
+    }
+
+
+}while(flag !=0);
+
+    }
+
+
+
+
+
+
+
+
+}
+
+
+
+
