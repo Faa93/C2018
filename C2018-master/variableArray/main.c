4083ee3d993f5326f28ab493213b1f8986668ae9
Faa93 <36281645+Faa93@users.noreply.github.com>
2018-09-04 21:56:32 -0300

    array

---
+++ C2018-master/variableArray/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define cant 5
+
+
+int main()
+{
+   int i;
+   int edad [cant];
+   float salario [cant];
+
+
+   for(i=0; i<cant; i++){
+    printf("Ingrese el edad: ");
+    scanf("%d", &edad[i]);
+   }
+   for(i=0;i<cant;i++){
+    printf("%d", edad[i]);
+   }
+ for()
+
+
+
+}
