9c16d007f7d7db288188fb1d7d99c257d896a639
Faa93 <36281645+Faa93@users.noreply.github.com>
2018-10-01 19:54:05 -0300

    Estructuras v3

---
+++ C2018-master/clase10/Estructuras/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*estructar
+    eEmpleado   ePais
+    dni         id (int)
+    apellido    description (char 31)
+    eFecha fNac
+    idPais (int) FK
+
+       -UNO A MUCHOS-
+
+
+
+
+       */
+
+}
